035292561a58c0cf289b4c8f6c01452952eb9e0e
Matthieu <matsvandamme@gmail.com>
2025-10-13 19:47:51 +0200

    Added enum to hold pin numbers

--- drivers/gpio.h
+++ drivers/gpio.h
@@ -1,25 +1,50 @@
 #ifndef GPIO_H
 #define GPIO_H
 
-// Define a struct to hold the direction value for the pin
+// Define an enum to hold the direction value for the pin
 typedef enum
 {
     INPUT,
     OUTPUT
 } gpio_direction_e;
 
-// Define a struct to hold the state of the pin resistor
+// Define an enum to hold the state of the pin resistor
 typedef enum
 {
     RESISTOR_ENABLED,
     RESISTOR_DISABLED
 } gpio_resistor_e;
 
-// Define a struct to hold the state of the pullup/pulldown
+// Define an enum to hold the state of the pullup/pulldown
 typedef enum
 {
     PULLUP,
     PULLDOWN
 } gpio_pullup_pulldown_e;
+
+// Define an enum to hold the pin numbers
+typedef enum
+{
+    GPIO0,
+    GPIO1,
+    GPIO2,
+    GPIO3,
+    GPIO4,
+    GPIO5,
+    GPIO6,
+    GPIO7,
+    GPIO8,
+    GPIO9,
+    GPIO10,
+    GPIO11,
+    GPIO12,
+    GPIO13,
+    GPIO14,
+    GPIO15,
+    GPIO16,
+    GPIO17,
+    GPIO18,
+    GPIO19
+} gpio_pins_e;
 
 #endif // GPIO_H
