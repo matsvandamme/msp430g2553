f508f78da5b994703f90e970b37c0f59c0c71436
Matthieu <matsvandamme@gmail.com>
2025-10-13 21:25:37 +0200

    implemented all gpio functions

--- drivers/gpio.c
+++ drivers/gpio.c
@@ -13,22 +13,22 @@ static volatile uint8_t *const port_in_regs[IO_PORT_CNT] = { &P1IN, &P2IN };
 static volatile uint8_t *const port_sel1_regs[IO_PORT_CNT] = { &P1SEL, &P2SEL };
 static volatile uint8_t *const port_sel2_regs[IO_PORT_CNT] = { &P1SEL2, &P2SEL2 };
 
-static uint8_t gpio_port(io_generic_e gpio)
+static uint8_t gpio_port(gpio_generic_e gpio)
 {
     return (gpio & IO_PORT_MASK) >> IO_PORT_OFFSET;
 }
 
-static inline uint8_t gpio_pin_idx(io_generic_e gpio)
+static inline uint8_t gpio_pin_idx(gpio_generic_e gpio)
 {
     return gpio & IO_PIN_MASK;
 }
 
-static uint8_t gpio_pin_bit(io_generic_e gpio)
+static uint8_t gpio_pin_bit(gpio_generic_e gpio)
 {
     return 1 << gpio_pin_idx(gpio);
 }
 
-void gpio_set_direction(io_generic_e gpio, io_dir_e direction)
+void gpio_set_direction(gpio_generic_e gpio, gpio_dir_e direction)
 {
     uint8_t port = gpio_port(gpio);
     uint8_t pin = gpio_pin_bit(gpio);
@@ -41,5 +41,67 @@ void gpio_set_direction(io_generic_e gpio, io_dir_e direction)
         case IO_DIR_OUTPUT:
             *port_dir_regs[port] |= pin;
             break;
-    };
-};
+    }
+}
+
+void io_set_resistor(gpio_generic_e gpio, gpio_resistor_e resistor)
+{
+    uint8_t port = gpio_port(gpio);
+    uint8_t pin = gpio_pin_bit(gpio);
+
+    switch(resistor)
+    {
+        case IO_RESISTOR_DISABLED:
+            *port_ren_regs[port] &= ~pin;
+            break;
+        case IO_RESISTOR_ENABLED:
+            *port_ren_regs[port] |= pin;
+            break;
+    } 
+}
+
+void io_set_select(gpio_generic_e gpio, gpio_select_e select)
+{
+    uint8_t port = gpio_port(gpio);
+    uint8_t pin = gpio_pin_bit(gpio);
+
+    switch(select)
+    {
+        case IO_SELECT_GPIO:
+            *port_sel1_regs[port] &= ~pin;
+            *port_sel2_regs[port] &= ~pin;
+            break;
+        case IO_SELECT_ALT1:
+            *port_sel1_regs[port] |= pin;
+            *port_sel2_regs[port] &= ~pin;
+            break;  
+        case IO_SELECT_ALT2:
+            *port_sel1_regs[port] &= ~pin;
+            *port_sel2_regs[port] |= pin;    
+            break;
+        case IO_SELECT_ALT3:
+            *port_sel1_regs[port] |= pin;
+            *port_sel2_regs[port] |= pin;
+            break;
+    }
+}
+
+void io_set_out(gpio_generic_e gpio, gpio_out_e out)
+{
+    uint8_t port = gpio_port(gpio);
+    uint8_t pin = gpio_pin_bit(gpio);
+
+    switch(out)
+    {
+        case IO_OUT_LOW:
+            *port_out_regs[port] &= ~pin;
+            break;
+        case IO_OUT_HIGH:
+            *port_out_regs[port] |= pin;
+    }
+}
+
+gpio_in_e io_get_input(gpio_generic_e gpio)
+{
+    return (*port_in_regs[gpio_port(gpio)] & gpio_pin_bit(gpio)) ? IO_IN_HIGH : IO_IN_LOW;
+}
--- drivers/gpio.h
+++ drivers/gpio.h
@@ -7,7 +7,7 @@
 typedef enum {
     IO_10, IO_11, IO_12, IO_13, IO_14, IO_15, IO_16, IO_17,
     IO_20, IO_21, IO_22, IO_23, IO_24, IO_25, IO_26, IO_27,
-} io_generic_e;
+} gpio_generic_e;
 
 typedef enum
 {
@@ -15,52 +15,52 @@ typedef enum
     IO_SELECT_ALT1,
     IO_SELECT_ALT2,
     IO_SELECT_ALT3,
-} io_select_e;
+} gpio_select_e;
 
 typedef enum
 {
     IO_DIR_INPUT,
     IO_DIR_OUTPUT,
-} io_dir_e;
+} gpio_dir_e;
 
 typedef enum
 {
     IO_RESISTOR_DISABLED,
     IO_RESISTOR_ENABLED,
-} io_resistor_e;
+} gpio_resistor_e;
 
 typedef enum
 {
     IO_OUT_LOW, // (pull-down)
     IO_OUT_HIGH, // (pull-up)
-} io_out_e;
+} gpio_out_e;
 
 typedef enum
 {
     IO_IN_LOW,
     IO_IN_HIGH,
-} io_in_e;
+} gpio_in_e;
 
 typedef enum
 {
     IO_TRIGGER_RISING,
     IO_TRIGGER_FALLING
-} io_trigger_e;
+} gpio_trigger_e;
 
 struct io_config
 {
-    io_select_e select;
-    io_resistor_e resistor;
-    io_dir_e dir;
-    io_out_e out;
+    gpio_select_e select;
+    gpio_resistor_e resistor;
+    gpio_dir_e dir;
+    gpio_out_e out;
 };
 
 void io_init(void);
-void io_configure(io_generic_e io, const struct io_config *config);
-void io_set_select(io_generic_e io, io_select_e select);
-void io_set_direction(io_generic_e io, io_dir_e direction);
-void io_set_resistor(io_generic_e io, io_resistor_e resistor);
-void io_set_out(io_generic_e io, io_out_e out);
-io_in_e io_get_input(io_generic_e io);
+void io_configure(gpio_generic_e gpio, const struct io_config *config);
+void io_set_select(gpio_generic_e gpio, gpio_select_e select);
+void io_set_direction(gpio_generic_e gpio, gpio_dir_e direction);
+void io_set_resistor(gpio_generic_e gpio, gpio_resistor_e resistor);
+void io_set_out(gpio_generic_e gpio, gpio_out_e out);
+gpio_in_e io_get_input(gpio_generic_e gpio);
 
 #endif // GPIO_H
