4169da94dca603f546df03edafb824b2e72c6db0
Matthieu <matsvandamme@gmail.com>
2025-10-13 19:39:33 +0200

    Changed struct to enum

--- drivers/gpio.h
+++ drivers/gpio.h
@@ -2,24 +2,24 @@
 #define GPIO_H
 
 // Define a struct to hold the direction value for the pin
-typedef struct
+typedef enum
 {
     INPUT,
     OUTPUT
-} gpio_direction_t;
+} gpio_direction_e;
 
 // Define a struct to hold the state of the pin resistor
-typedef struct
+typedef enum
 {
     RESISTOR_ENABLED,
     RESISTOR_DISABLED
-} gpio_resistor_t;
+} gpio_resistor_e;
 
 // Define a struct to hold the state of the pullup/pulldown
-typedef struct gpio
+typedef enum
 {
     PULLUP,
     PULLDOWN
-} gpio_pullup_pulldown_t;
+} gpio_pullup_pulldown_e;
 
 #endif // GPIO_H
